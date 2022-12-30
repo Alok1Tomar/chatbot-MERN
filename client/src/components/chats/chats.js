eea388ea588a7343d281a79a020ab9e2849be4a2
Alok1Tomar <tomaralok2000@gmail.com>
2022-12-30 12:29:44 +0530

    first commit

---
+++ client/src/components/chats/chats.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+
+
+
+export class Chat extends Component {
+    componentDidMount(){
+        
+    (function(d, m){
+        var kommunicateSettings = 
+            {"appId":"2d1e0070c0e8eb121a539cd91a3d529fb","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+/* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
+
+    }
+  render() {
+    return (
+      <div>
+      
+    </div>
+
+      
+      
+ 
+    
+    
+    )
+  }
+}
+
+export default Chat
